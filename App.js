6b273eb7484f825dce17ba6b4a696eea94d956fa
[jwhong70] <[jwhong70@gmail.com]>
2022-12-03 18:57:23 +0900

    #1-2 Drag and Drop 1

--- App.js
+++ App.js
@@ -1,6 +1,98 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components/native";
+import { Animated, View, PanResponder } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
+const BLACK_COLOR = "#1e272e";
+const GREY = "#485460";
+const GREEN = "#2ecc71";
+const RED = "#e74c3c";
+const Container = styled.View`
+  background-color: ${BLACK_COLOR};
+  flex: 1;
+`;
+const Edge = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+const Center = styled.View`
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+  flex: 3;
+`;
+const WordContainer = styled(Animated.createAnimatedComponent(View))`
+  width: 100px;
+  height: 100px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${GREY};
+  border-radius: 50px;
+`;
+const Word = styled.Text`
+  font-size: 38px;
+  font-weight: 500;
+  color: ${(props) => props.color};
+`;
+const IconCard = styled(Animated.createAnimatedComponent(View))`
+  background-color: white;
+  padding: 10px 20px;
+  border-radius: 10px;
+  z-index: 10;
+  position: absolute;
+`;
 export default function App() {
-  return null;
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        onPressIn.start();
+      },
+      onPanResponderMove: (_, { dx, dy }) => {
+        position.setValue({ x: dx, y: dy });
+      },
+      onPanResponderRelease: () => {
+        Animated.parallel([onPressOut, goHome]).start();
+      },
+    })
+  ).current;
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.9,
+    useNativeDriver: true,
+  });
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
+  const goHome = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
+  return (
+    <Container>
+      <Edge>
+        <WordContainer>
+          <Word color={GREEN}>알아</Word>
+        </WordContainer>
+      </Edge>
+      <Center>
+        <IconCard
+          {...panResponder.panHandlers}
+          style={{
+            transform: [...position.getTranslateTransform(), { scale }],
+          }}
+        >
+          <Ionicons name="beer" color={GREY} size={76} />
+        </IconCard>
+      </Center>
+      <Edge>
+        <WordContainer>
+          <Word color={RED}>몰라</Word>
+        </WordContainer>
+      </Edge>
+    </Container>
+  );
 }
